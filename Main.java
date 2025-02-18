093354b76865517edfdb06fb8a6b2d92110250d8
junbaipak <octaves-chervil-9y@icloud.com>
2025-02-18 20:52:53 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,95 +1,70 @@
-class Book {
-    protected String title;
-    protected String author;
-    protected int publishedYear;
+abstract class Employee {
+    String name;
+    int id;
+    double baseSalary;
 
-    public Book (String title, String author, int publishedYear) {
-        this.title = title;
-        this.author = author;
-        this.publishedYear = publishedYear;
+    public Employee (String name, int id, double baseSalary) {
+        this.name = name;
+        this.id = id;
+        this.baseSalary = baseSalary;
     }
 
+    public abstract double calcSalary ();
 
-    public String getBookInfo () {
-        return "Author " + author + ". Title: " + title + ". Year published: " + publishedYear + ".";
-    }
-
-    public void printBookDetails () {
-        System.out.println(getBookInfo());
-    }
-
-
-}
-
-class PrintedBook extends Book {
-
-    protected int numOfPages;
-    protected String publisher;
-
-    public PrintedBook(String title, String author, int publishedYear, int numOfPages, String publisher) {
-        super(title, author, publishedYear);
-        this.numOfPages = numOfPages;
-        this.publisher = publisher;
-    }
-
-    @Override
-    public String getBookInfo () {
-        return super.getBookInfo() + ". Pages" + numOfPages + ". Publisher: " + publisher;
-    }
-
-    @Override
-    public void printBookDetails () {
-        System.out.println(getBookInfo());
-    }
-
-    public void BookType () {
-        System.out.println("This is a printed book");
-    }
-
-}
-
-class EBook extends Book {
-    protected double fileSizeMB;
-    protected String fileFormat;
-
-    public EBook (String title, String author, int publishedYear, double fileSizeMB, String fileFormat) {
-        super(title, author, publishedYear);
-        this.fileSizeMB = fileSizeMB;
-        this.fileFormat = fileFormat;
-    }
-
-    @Override
-    public String getBookInfo() {
-        return super.getBookInfo() + ". File size: " + fileSizeMB + ". File Format: " + fileFormat;
-    }
-
-    @Override
-    public void printBookDetails (){
-        System.out.println(getBookInfo());
-    }
-
-    public void BookType () {
-        System.out.println("This is an ebook");
+    public void displayEmployeeInfo () {
+        System.out.println(name + " " + id + " " + baseSalary);
     }
 }
 
+interface Payable {
+    double getPaymentAmount() ;
+}
 
+class FullTimeEmployee extends Employee implements Payable {
+    public FullTimeEmployee(String name, int id, double baseSalary) {
+        super(name, id, baseSalary*1.2);
+    }
 
+    @Override
+    public double calcSalary() {
+        return getPaymentAmount();
+    }
 
+    @Override
+    public double getPaymentAmount() {
+        return baseSalary;
+    }
+}
+
+class ContractEmployee extends Employee implements Payable {
+    private int hourlyRate;
+    private int hoursWorked;
+
+    public ContractEmployee(String name, int id, double baseSalary, int hourlyRate, int hoursWorked) {
+        super(name, id, baseSalary);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calcSalary() {
+        return getPaymentAmount();
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return hourlyRate * hoursWorked;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        Book b1 = new Book("Jamilya", "Aitmatov", 2024);
-        System.out.println(b1.getBookInfo());
-        b1.printBookDetails();
-
-        PrintedBook b2 = new PrintedBook("Plaha", "Aitmatov", 2023, 2000, "Baistan");
-        b2.getBookInfo();
-        b2.printBookDetails();
-        b2.BookType();
-
-        EBook b3 = new EBook("Gory", "Aitmatov", 2020, 8.0, "pdf");
-        b3.printBookDetails();
-        b3.BookType();
+        Employee[] emps = new Employee[2];
+        emps[0] = new FullTimeEmployee("Baistan", 123, 500000);
+        emps[1] = new ContractEmployee("Akbar", 456, 500, 2, 200);
+        for(Employee e : emps) {
+            e.displayEmployeeInfo();
+            System.out.println(e.calcSalary());
+        }
     }
 }
